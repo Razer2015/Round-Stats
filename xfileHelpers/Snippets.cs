839a7a80cbfe53c215577ad54546c587dc053c2c
Razer2015 <razer138@gmail.com>
2016-12-11 14:27:08 +0200

    Alpha version
    Added the missing files.

---
+++ xfileHelpers/Snippets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+
+/// <summary>
+/// Additional Helpers
+/// </summary>
+public class Snippets
+{
+    /// <summary>
+    /// Decode URL content
+    /// </summary>
+    /// <param name="encoded"></param>
+    /// <returns></returns>
+    public static String HTMLDecode(string encoded)
+    {
+        return (HttpUtility.HtmlDecode(encoded));
+    }
+
+    /// <summary>
+    /// Encode URL content
+    /// </summary>
+    /// <param name="decoded"></param>
+    /// <returns></returns>
+	public static String HTMLEncode(string decoded)
+    {
+        return (HttpUtility.HtmlEncode(decoded));
+    }
+}
